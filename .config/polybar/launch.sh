a686c8f3bf99ce48865b9b574cea76996001492e
FlorentCollin <florent.collin23@gmail.com>
2019-06-05 12:40:01 +0200

    Polybar modification

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,13 @@
+# Terminate already running instances of the bar
+killall -q polybar
+
+# Wait for the processes to shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main &
+  done
+else
+  polybar --reload example &
+fi
