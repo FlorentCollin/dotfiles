5f9ec90f31473ee4696757f3bcf6adfeaa2e198c
FlorentCollin <florent.collin23@gmail.com>
2019-06-24 22:35:32 +0000

    Fix symlink script

--- install.sh
+++ install.sh
@@ -3,14 +3,15 @@ shopt -s dotglob
 
 for file in *; do
   echo "Creating SymLink for $file"
-  ln -sf $file $HOME/
+  ln -sf $PWD/$file $HOME/
 done
 
 for file in .config/*; do
   echo "Creating SymLink for .config/$file"
-  ln -sf $file $HOME/.config/
+  ln -sf $PWD/$file $HOME/.config/
 done
 
-echo "Removing symlink for the install file
+echo "Removing symlink for the install file"
 unlink $HOME/install.sh
+unlink $HOME/.git
 
